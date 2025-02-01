e72d57c9d1857b072a3e70e3dec311b841086d92
Bennet Becker <dev@bennet.cc>
2025-02-01 19:17:41 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# This is what your ‘setup.py’ file should look like.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mailman_sender_plugin", #Name
+    version="1.0", #Version
+    packages = find_packages()  # Automatically find the packages that are recognized in the '__init__.py'.
+)
