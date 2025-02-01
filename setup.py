b51d69373b7f4b9533f30ffc7e5251e18eaeb5cc
Bennet Becker <dev@bennet.cc>
2025-02-01 19:26:52 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from setuptools import setup, find_packages
 setup(
     name="mailman_sender_plugin", #Name
     version="1.0", #Version
-    packages = ["mailman_sender_plugin"],
+    packages = ["mailman_sender_plugin", "mailman_sender_plugin.pipelines", "mailman_sender_plugin.handlers"],
     package_dir = {"mailman_sender_plugin":"."}
 )
